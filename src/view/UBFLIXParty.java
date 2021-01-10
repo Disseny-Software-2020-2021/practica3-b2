adc61afacf031a3efd4a3dc2de57718d0e3e98c9
GerardLopezRuiz <gerardlopez201@gmail.com>
2021-01-10 18:36:56 +0100

    comitt

--- src/view/UBFLIXParty.java
+++ src/view/UBFLIXParty.java
@@ -126,7 +126,6 @@ public class UBFLIXParty extends JFrame{
             }
         });
         popupMenuTemporades = new HashMap<>();
-        comboBoxUsuaris.addItem("Pol");
         inicialitzarLlistaTopVisualitzacions();
         inicialitzarLlistaTopValoracions();
     }
